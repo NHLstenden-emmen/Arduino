077ad415480adb1b3bb0a8d8ac63d25859156314
Tomdegr <73107658+Tomdegr@users.noreply.github.com>
2021-02-18 10:03:44 +0100

    Update lcd_test.ino

--- lcd_test/lcd_test.ino
+++ lcd_test/lcd_test.ino
@@ -6,11 +6,11 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 void setup()
 {
   lcd.init();                      // initialize the lcd 
-  lcd.init();
-  // Print a message to the LCD.
+  
+                                  // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(1,0);
-  lcd.print("hello everyone");
+  lcd.print("Hallo allemaal");
 }
 
 
