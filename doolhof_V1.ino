a0a1bfc601cddba2cff650639dce01130a734c00
Kevinsmul <smulders.kevin.1999@gmail.com>
2021-03-23 09:35:14 +0100

    Create doolhof_V1.ino

---
+++ doolhof_V1.ino
@@ -0,0 +1,163 @@
+#include <Wire.h> 
+
+#define V_motor_L   16           // vooruit motor links
+#define A_motor_L   17           // Achteruit motor links
+
+
+#define V_motor_R   18          // vooruit motor Rechts
+#define A_motor_R   5           // Achteruit motor Rechts
+
+
+#define IR_Rechts 34          // Linker IR sensor 
+#define IR_Links  39          // Rechter IR sensor
+
+int  IR_Rechts_val = 0;
+int  IR_Links_val = 0;
+
+const int freq = 3000;
+const int SV_motor_L = 0;
+const int SA_motor_L = 1;
+const int SV_motor_R = 2;
+const int SA_motor_R = 3;
+const int resolution = 8;
+int Sens = 500;
+int RandVal = 0;
+
+void setup() 
+{
+  pinMode(V_motor_L, OUTPUT);
+  pinMode(A_motor_L , OUTPUT);
+
+  pinMode(V_motor_R, OUTPUT);
+  pinMode(A_motor_R , OUTPUT);
+  
+  pinMode(IR_Rechts, INPUT);
+  pinMode(IR_Links, INPUT);
+  Serial.begin(9600);
+
+  ledcSetup(SV_motor_L, freq, resolution);
+  ledcAttachPin(V_motor_L, SV_motor_L);
+
+  ledcSetup(SA_motor_L, freq, resolution);
+  ledcAttachPin(A_motor_L, SA_motor_L);
+
+  ledcSetup(SV_motor_R, freq, resolution);
+  ledcAttachPin(V_motor_R, SV_motor_R);
+
+  ledcSetup(SA_motor_R, freq, resolution);
+  ledcAttachPin(A_motor_R, SA_motor_R);
+}
+
+void loop() 
+{
+  if(IR_Rechts_val < Sens && IR_Links_val < Sens)
+  {
+    ledcWrite(SV_motor_L, 120);
+    ledcWrite(SA_motor_L, 0);
+  
+    ledcWrite(SV_motor_R, 120);
+    ledcWrite(SA_motor_R, 0);
+
+    while (IR_Rechts_val < Sens && IR_Links_val < Sens)
+    {
+      IR_Rechts_val = analogRead(IR_Rechts);
+      IR_Links_val = analogRead(IR_Links);
+      Serial.println(IR_Rechts_val);
+      delay(10);
+    }
+  }
+   
+    else if(IR_Rechts_val < Sens && IR_Links_val > Sens)
+    {
+      ledcWrite(SV_motor_L, 0);
+      ledcWrite(SA_motor_L, 120);
+  
+      ledcWrite(SV_motor_R, 0);
+      ledcWrite(SA_motor_R, 120);
+      
+      delay(750);
+
+      ledcWrite(SV_motor_L, 180);
+      ledcWrite(SA_motor_L, 0);
+  
+      ledcWrite(SV_motor_R, 80);
+      ledcWrite(SA_motor_R, 0);
+
+      delay(750);
+      
+      while (IR_Rechts_val < Sens && IR_Links_val > Sens)
+        {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+        }
+    }
+    else if (IR_Rechts_val > Sens && IR_Links_val < Sens)
+    {
+      ledcWrite(SV_motor_L, 0);
+      ledcWrite(SA_motor_L, 120);
+  
+      ledcWrite(SV_motor_R, 0);
+      ledcWrite(SA_motor_R, 120);
+      
+      delay(750);
+
+      ledcWrite(SV_motor_L, 80);
+      ledcWrite(SA_motor_L, 0);
+  
+      ledcWrite(SV_motor_R, 180);
+      ledcWrite(SA_motor_R, 0);
+      
+      while (IR_Rechts_val > Sens && IR_Links_val < Sens)
+      {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+      }   
+    }
+    else if (IR_Rechts_val > Sens && IR_Links_val > Sens)
+    {
+      ledcWrite(SV_motor_L, 0);
+      ledcWrite(SA_motor_L, 120);
+  
+      ledcWrite(SV_motor_R, 0);
+      ledcWrite(SA_motor_R, 120);
+      
+      delay(750);
+
+      RandVal = random(0,3);
+
+        if(RandVal == 1)
+        {
+          ledcWrite(SV_motor_L, 180);
+          ledcWrite(SA_motor_L, 0);
+  
+          ledcWrite(SV_motor_R, 80);
+          ledcWrite(SA_motor_R, 0);
+          delay(750);
+        }
+        else if (RandVal == 2)
+        {
+          ledcWrite(SV_motor_L, 80);
+          ledcWrite(SA_motor_L, 0);
+  
+          ledcWrite(SV_motor_R, 180);
+          ledcWrite(SA_motor_R, 0);
+          delay(750);
+        }
+        else
+        {
+          Serial.println("error");
+        }
+       while (IR_Rechts_val > Sens && IR_Links_val > Sens)
+        {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+        }
+    }
+    delay(10);
+}
