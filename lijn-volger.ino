a648b237a6a5b639817ffe5fffbf3ba3a086baf3
Kevinsmul <smulders.kevin.1999@gmail.com>
2021-03-23 09:33:35 +0100

    Update lijn-volger.ino

--- lijn-volger.ino
+++ lijn-volger.ino
@@ -1,145 +1,117 @@
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+#define V_motor_L   16           // vooruit motor links
+#define A_motor_L   17           // Achteruit motor links
 
-#define S_motor_L  6           // snelheid motor links
-#define V_motor_L  2          // vooruit motor links
-#define A_motor_L  3          // Richting Rechts motor links
 
-#define S_motor_R  5           // snelheid motor Rechts
-#define V_motor_R  9          // Richting Links motor Rechts
-#define A_motor_R  4          // Richting Rechts motor Rechts
+#define V_motor_R   18          // vooruit motor Rechts
+#define A_motor_R   5           // Achteruit motor Rechts
 
-#define IR_Rechts 10
-#define IR_Links  11
+
+#define IR_Rechts 34          // Linker IR sensor 
+#define IR_Links  39          // Rechter IR sensor
 
 int  IR_Rechts_val = 0;
 int  IR_Links_val = 0;
+
+const int freq = 3000;
+const int SV_motor_L = 0;
+const int SA_motor_L = 1;
+const int SV_motor_R = 2;
+const int SA_motor_R = 3;
+const int resolution = 8;
+int Sens = 500;
 
 void setup() 
 {
   pinMode(V_motor_L, OUTPUT);
   pinMode(A_motor_L , OUTPUT);
-  pinMode(S_motor_L, OUTPUT);
 
   pinMode(V_motor_R, OUTPUT);
   pinMode(A_motor_R , OUTPUT);
-  pinMode(S_motor_R, OUTPUT);
   
   pinMode(IR_Rechts, INPUT);
   pinMode(IR_Links, INPUT);
   Serial.begin(9600);
+
+  ledcSetup(SV_motor_L, freq, resolution);
+  ledcAttachPin(V_motor_L, SV_motor_L);
+
+  ledcSetup(SA_motor_L, freq, resolution);
+  ledcAttachPin(A_motor_L, SA_motor_L);
+
+  ledcSetup(SV_motor_R, freq, resolution);
+  ledcAttachPin(V_motor_R, SV_motor_R);
+
+  ledcSetup(SA_motor_R, freq, resolution);
+  ledcAttachPin(A_motor_R, SA_motor_R);
 }
 
 void loop() 
 {
-  if(IR_Rechts_val == 0 && IR_Links_val == 0)
+  if(IR_Rechts_val < Sens && IR_Links_val < Sens)
   {
-   lcd.init();
-   lcd.backlight();
-   lcd.setCursor(0,1);
-   lcd.print(IR_Rechts_val);
-   lcd.setCursor(15,1);
-   lcd.print(IR_Links_val); 
-   lcd.setCursor(3,0);
-   lcd.print("rechtdoor!");
+    ledcWrite(SV_motor_L, 120);
+    ledcWrite(SA_motor_L, 0);
+  
+    ledcWrite(SV_motor_R, 120);
+    ledcWrite(SA_motor_R, 0);
 
-    digitalWrite(V_motor_L,HIGH);
-    digitalWrite(A_motor_L,LOW);
-    analogWrite(S_motor_L , 60);
-
-    digitalWrite(V_motor_R,HIGH);
-    digitalWrite(A_motor_R,LOW);
-    analogWrite(S_motor_R , 70);
-    
-   while (IR_Rechts_val == 0 && IR_Links_val == 0)
-   {
-     IR_Rechts_val = digitalRead(IR_Rechts);
-     IR_Links_val = digitalRead(IR_Links);
-     //Serial.println(IR_Rechts_val);
-     delay(50);
-   }
+    while (IR_Rechts_val < Sens && IR_Links_val < Sens)
+    {
+      IR_Rechts_val = analogRead(IR_Rechts);
+      IR_Links_val = analogRead(IR_Links);
+      Serial.println(IR_Rechts_val);
+      delay(10);
+    }
   }
    
-    else if(IR_Rechts_val == 1 && IR_Links_val == 0)
-    {
-       lcd.setCursor(0,1);
-       lcd.print(IR_Rechts_val);
-       lcd.setCursor(15,1);
-       lcd.print(IR_Links_val);
-       lcd.setCursor(3,0);
-       lcd.print("stuur links!");
-       
-    digitalWrite(V_motor_L,LOW);
-    digitalWrite(A_motor_L,LOW);
-    analogWrite(S_motor_L , 0);
-
-    digitalWrite(V_motor_R,LOW);
-    digitalWrite(A_motor_R,LOW);
-    analogWrite(S_motor_R , 0);
-
-     while (IR_Rechts_val == 1 && IR_Links_val == 0)
-   {
-     IR_Rechts_val = digitalRead(IR_Rechts);
-     IR_Links_val = digitalRead(IR_Links);
-     //Serial.println(IR_Rechts_val);
-     delay(50);
-   }
-    }
-    else if (IR_Rechts_val == 0 && IR_Links_val == 1)
+    else if(IR_Rechts_val < Sens && IR_Links_val > Sens)
     {
 
-       lcd.setCursor(0,1);
-       lcd.print(IR_Rechts_val);
-       lcd.setCursor(15,1);
-       lcd.print(IR_Links_val);
-       lcd.setCursor(3,0);
-       lcd.print("stuur rechts!");
-
-       digitalWrite(V_motor_L,LOW);
-       digitalWrite(A_motor_L,LOW);
-       analogWrite(S_motor_L , 0);
-
-       digitalWrite(V_motor_R,LOW);
-       digitalWrite(A_motor_R,LOW);
-       analogWrite(S_motor_R , 0);
-
-      while (IR_Rechts_val == 0 && IR_Links_val == 1)
-      {
-        IR_Rechts_val = digitalRead(IR_Rechts);
-         IR_Links_val = digitalRead(IR_Links);
-        //Serial.println(IR_Rechts_val);
-         delay(50);
-      }
-    }
-    else
-    {
-       lcd.init();
-       lcd.backlight();
-       lcd.setCursor(0,1);
-       lcd.print(IR_Rechts_val);
-       lcd.setCursor(15,1);
-       lcd.print(IR_Links_val); 
-       lcd.setCursor(3,0);
-       lcd.print("Stop!");
-
-       digitalWrite(V_motor_L,LOW);
-       digitalWrite(A_motor_L,LOW);
-       analogWrite(S_motor_L , 0);
-
-       digitalWrite(V_motor_R,LOW);
-       digitalWrite(A_motor_R,LOW);
-       analogWrite(S_motor_R , 0);
-
-       while (IR_Rechts_val == 1 && IR_Links_val == 1)
-      {
-        IR_Rechts_val = digitalRead(IR_Rechts);
-         IR_Links_val = digitalRead(IR_Links);
-       // Serial.println(IR_Rechts_val);
-         delay(50);
-      }
-    }
+      ledcWrite(SV_motor_L, 180);
+      ledcWrite(SA_motor_L, 0);
   
+      ledcWrite(SV_motor_R, 80);
+      ledcWrite(SA_motor_R, 0);
+
+      while (IR_Rechts_val < Sens && IR_Links_val > Sens)
+        {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+        }
+    }
+    else if (IR_Rechts_val > Sens && IR_Links_val < Sens)
+    {
+      ledcWrite(SV_motor_L, 80);
+      ledcWrite(SA_motor_L, 0);
+      ledcWrite(SV_motor_R, 180);
+      ledcWrite(SA_motor_R, 0);
+      
+      while (IR_Rechts_val > Sens && IR_Links_val < Sens)
+      {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+      }   
+    }
+    else if (IR_Rechts_val > Sens && IR_Links_val > Sens)
+    {
+      ledcWrite(SV_motor_L, 0);
+      ledcWrite(SA_motor_L, 0);
+      ledcWrite(SV_motor_R, 0);
+      ledcWrite(SA_motor_R, 0);
+      
+       while (IR_Rechts_val > Sens && IR_Links_val > Sens)
+        {
+          IR_Rechts_val = analogRead(IR_Rechts);
+          IR_Links_val = analogRead(IR_Links);
+          Serial.println(IR_Rechts_val);
+          delay(10);
+        }
+    }
     delay(10);
 }
