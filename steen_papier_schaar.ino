ccd1691c546d7d716097ede470f517866adeb85e
Kevinsmul <smulders.kevin.1999@gmail.com>
2021-03-23 09:31:48 +0100

    Create steen_papier_schaar.ino

---
+++ steen_papier_schaar.ino
@@ -0,0 +1,71 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int number = 0;
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display();
+  
+}
+
+void loop() {
+  number = random(1,4);
+
+Serial.println("-----------");
+Serial.println(number);
+if (number == 1){
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+   display.println("Ik kies papier.");
+   display.display(); 
+   }
+
+  if (number == 2){
+   delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+   display.println("Ik kies steen.");
+   display.display(); 
+   
+  }
+
+  if (number == 3){
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+   display.println("Ik kies schaar.");
+   display.display(); 
+   }
+delay(3000);
+}
